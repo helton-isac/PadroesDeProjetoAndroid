31cb23072bf65a65b8f664705ebdc3fbc7b9f04a
Helton Isac <helton.isac@gmail.com>
2020-10-20 20:25:16 -0300

    Creating Contract

---
+++ app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainContract.kt
@@ -0,0 +1,14 @@
+package com.hitg.padroesdeprojetoandroid.ui.main
+
+import com.hitg.padroesdeprojetoandroid.model.Endereco
+
+interface MainContract {
+    interface MainView {
+        fun mostrarEndereco(endereco: Endereco)
+        fun mostrarErro(mensagem: String)
+    }
+
+    interface MainPresenter {
+        fun pesquisar(cep: String)
+    }
+}
