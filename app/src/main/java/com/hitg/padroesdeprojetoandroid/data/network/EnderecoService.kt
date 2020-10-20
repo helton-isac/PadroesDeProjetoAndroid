f9d6d9d3adaef82ac0b31d3a7388b63036e8900e
Helton Isac <helton.isac@gmail.com>
2020-10-20 20:07:05 -0300

    Implementing the search on mvc

--- app/src/main/java/com/hitg/padroesdeprojetoandroid/data/network/EnderecoService.kt
+++ app/src/main/java/com/hitg/padroesdeprojetoandroid/data/network/EnderecoService.kt
@@ -3,11 +3,11 @@ package com.hitg.padroesdeprojetoandroid.data.network
 import com.hitg.padroesdeprojetoandroid.model.Endereco
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface EnderecoService {
 
     @GET("/ws/{cep}/json")
-    fun pesquisar(cep: String): Call<Endereco>
-
+    fun pesquisar(@Path("cep") cep: String): Call<Endereco>
 
 }
--- app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainActivity.kt
@@ -1,13 +1,57 @@
 package com.hitg.padroesdeprojetoandroid.ui.main
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hitg.padroesdeprojetoandroid.R
+import com.hitg.padroesdeprojetoandroid.data.network.APIService
+import com.hitg.padroesdeprojetoandroid.model.Endereco
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btPesquisar.setOnClickListener {
+            pesquisar()
+        }
+    }
+
+    private fun pesquisar() {
+        APIService.instance
+            ?.pesquisar(etCEP.text.toString())
+            ?.enqueue(object : Callback<Endereco> {
+                override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+                    if (response.isSuccessful) {
+                        val endereco = response.body()
+                        endereco?.let {
+                            tvLogradouro.text = endereco.logradouro
+                            tvBairro.text = endereco.bairro
+                            tvLocalidade.text = endereco.localidade
+                            tvUF.text = endereco.uf
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Endereço não encontrado",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<Endereco>, t: Throwable) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        t.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            })
     }
 }
