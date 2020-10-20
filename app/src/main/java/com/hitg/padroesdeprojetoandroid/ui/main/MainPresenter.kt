8d3d80be761bb0a0d2dcf77f253cf72df0ea182d
Helton Isac <helton.isac@gmail.com>
2020-10-20 20:45:54 -0300

    Validating empty cep inside presenter

--- app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainPresenter.kt
+++ app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainPresenter.kt
@@ -8,20 +8,24 @@ import retrofit2.Response
 
 class MainPresenter(var view: MainContract.MainView) : MainContract.MainPresenter {
     override fun pesquisar(cep: String) {
-        APIService.instance
-            ?.pesquisar(cep)
-            ?.enqueue(object : Callback<Endereco> {
-                override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
-                    if (response.isSuccessful) {
-                        view.mostrarEndereco(response.body())
-                    } else {
-                        view.mostrarErro("Endereço não encontrado")
+        if (cep.isEmpty()) {
+            view.mostrarErro("Informe o CEP")
+        } else {
+            APIService.instance
+                ?.pesquisar(cep)
+                ?.enqueue(object : Callback<Endereco> {
+                    override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+                        if (response.isSuccessful) {
+                            view.mostrarEndereco(response.body())
+                        } else {
+                            view.mostrarErro("Endereço não encontrado")
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<Endereco>, t: Throwable) {
-                    view.mostrarErro(t.message.toString())
-                }
-            })
+                    override fun onFailure(call: Call<Endereco>, t: Throwable) {
+                        view.mostrarErro(t.message.toString())
+                    }
+                })
+        }
     }
 }
