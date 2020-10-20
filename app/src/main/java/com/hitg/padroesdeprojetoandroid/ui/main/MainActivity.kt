3ae932eaf3a927b6f52c1450bd881642699e847f
Helton Isac <helton.isac@gmail.com>
2020-10-20 20:40:40 -0300

    Implementing View

--- app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.hitg.padroesdeprojetoandroid.ui.main
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hitg.padroesdeprojetoandroid.R
 import com.hitg.padroesdeprojetoandroid.model.Endereco
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainContract.MainView {
+
+    private lateinit var mainPresenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainPresenter = MainPresenter(this)
+
+        btPesquisar.setOnClickListener {
+            mainPresenter.pesquisar(etCEP.text.toString())
+        }
     }
 
     override fun mostrarEndereco(endereco: Endereco?) {
-        TODO("Not yet implemented")
+        endereco?.let {
+            tvLogradouro.text = endereco.logradouro
+            tvBairro.text = endereco.bairro
+            tvLocalidade.text = endereco.localidade
+            tvUF.text = endereco.uf
+        }
     }
 
     override fun mostrarErro(mensagem: String) {
-        TODO("Not yet implemented")
+        Toast.makeText(
+            this@MainActivity,
+            mensagem,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
