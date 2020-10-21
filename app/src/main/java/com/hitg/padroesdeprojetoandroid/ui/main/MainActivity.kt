4655edc7d2f400a2c9082f38cd894c406107c1b1
Helton Isac <helton.isac@gmail.com>
2020-10-20 23:03:07 -0300

    Implementing DataBind

--- app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/hitg/padroesdeprojetoandroid/ui/main/MainActivity.kt
@@ -3,10 +3,11 @@ package com.hitg.padroesdeprojetoandroid.ui.main
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hitg.padroesdeprojetoandroid.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.hitg.padroesdeprojetoandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,24 +15,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        registerObserver()
+        initDataBind()
 
-        btPesquisar.setOnClickListener {
-            mainViewModel.pesquisar(etCEP.text.toString())
-        }
+        registerObserver()
+    }
+
+    private fun initDataBind() {
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
     }
 
     private fun registerObserver() {
-        mainViewModel.enderecoResponse.observe(this, Observer {
-            tvLogradouro.text = it.logradouro
-            tvBairro.text = it.bairro
-            tvLocalidade.text = it.localidade
-            tvUF.text = it.uf
-        })
         mainViewModel.mensagemErro.observe(this, Observer {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
